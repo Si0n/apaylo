bb6d3984be6ca87e2e60874f6cee5ad4e03a4083
sion <sionix1@gmail.com>
2025-03-13 16:44:19 +0100

    Add new statuses for InteracTransactionStatus enum
    Introduced PENDING and REQUESTED statuses to the InteracTransactionStatus enum, along with their corresponding text representations and descriptions. This enhances the granularity and clarity of transaction states in the system.

--- src/Enum/Interac/InteracTransactionStatus.php
+++ src/Enum/Interac/InteracTransactionStatus.php
@@ -6,6 +6,8 @@ enum InteracTransactionStatus: string
 {
     case INCOMPLETE = 'Incomplete';
     case PENDING_REQUESTED = 'Pending/Requested';
+    case PENDING = 'Pending';
+    case REQUESTED = 'Requested';
     case AUTHENTICATED = 'Authenticated';
     case CANCELLED = 'Cancelled';
     case SENT = 'Sent';
@@ -17,6 +19,8 @@ enum InteracTransactionStatus: string
 
     private const string TEXT_INCOMPLETE = 'Incomplete';
     private const string TEXT_PENDING_REQUESTED = 'Pending/Requested';
+    private const string TEXT_PENDING = 'Pending';
+    private const string TEXT_REQUESTED = 'Requested';
     private const string TEXT_AUTHENTICATED = 'Authenticated';
     private const string TEXT_CANCELLED = 'Cancelled';
     private const string TEXT_SENT = 'Sent';
@@ -39,6 +43,8 @@ enum InteracTransactionStatus: string
         return match ($this) {
             self::INCOMPLETE => 'Transaction failed to be created',
             self::PENDING_REQUESTED => 'Transaction has been created/registered',
+            self::PENDING => 'Transaction is pending',
+            self::REQUESTED => 'Transaction has been requested',
             self::AUTHENTICATED => 'Secret Answer has been properly provided',
             self::CANCELLED => 'Merchant has cancelled the transaction, or the transaction has expired',
             self::SENT => 'Transaction has been sent',
